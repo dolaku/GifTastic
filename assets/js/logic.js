5892d84214db3defefbf4a74e1f7e41a72d277d6
dolaku <dolaku@gmail.com>
2019-04-04 19:17:22 -0400

    Buttons updates ajax call

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-    var items = ['apple', 'banana', 'pie', 'sandwich', 'pizza', 'cake', 'donut', 'steak', 'cheese', 'broccoli', 'avocado', 'ice cream', 'cabbage', 'carrots', 'bacon'];
+    var items = ['banana', 'pie', 'sandwich', 'pizza', 'donut', 'steak', 'cheese', 'broccoli', 'avocado', 'ice cream', 'bacon', 'taco', 'cookie','egg'];
 
-
+    var searchTerm;
 
     // Display items to DOM
     displayItems();
 
     // loop through items array to create a list
-    function displayItems(){
+    function displayItems() {
         $('#items-wrapper').empty();
 
         for (var i = 0; i < items.length; i++) {
@@ -24,23 +24,28 @@ $(document).ready(function () {
     }
 
 
-    // Event listeners for submit button && enter key
+    // Event listeners - submit button && enter key
     $("#submit-btn").on("click", function () {
-        searchGiphy();
-        displayItems();
+        runSearch();
     });
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
             // Prevent page from refreshing
             e.preventDefault();
-            searchGiphy();
-            displayItems();
+            runSearch();
         }
     });
 
 
-    // change state of gif - static or active
+    // Listener - search with item buttons
+    $(document).on('click', '.item-btn', function () {
+        var btnName = $(this).attr('data-name');
+        callAPI(btnName);
+    });
+
+
+    // Listener - change state of gif - static or active
     $(document).on('click', '.gif', function () {
         var state = $(this).attr("data-state");
 
@@ -55,32 +60,40 @@ $(document).ready(function () {
         }
     });
 
+    function runSearch() {
+        searchTerm = $('#add-item').val().trim();
+        callAPI(searchTerm);
+        displayItems();
+        $('#add-item').empty()
+    }
 
+    // function searchGiphy() {
+    //     callAPI(searchTerm);
+    // }
+    
+    
     // ajax call
-    function searchGiphy() {
-        var addItem = $('#add-item').val().trim();
-
+    function callAPI(keyword) {
         // GIPHY API key: jKzhnrJqtR9l5EHmGMzKHL22x5fbUIDA
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            addItem + "&api_key=jKzhnrJqtR9l5EHmGMzKHL22x5fbUIDA";
-
+        keyword + "&api_key=jKzhnrJqtR9l5EHmGMzKHL22x5fbUIDA";
+        
         $('#gallery').empty();
-
+                
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
             var results = response.data;
-
+    
             for (var i = 0; i < results.length; i++) {
                 var result = results[i];
-                var state = 'static';
                 var gifStatic = result.images['480w_still'].url;
                 var gifActive = result.images.original.url;
                 var rating = result.rating.toUpperCase();
                 var title = result.title;
-
+    
                 // update DOM -- add gifs to the gallery
                 $('#gallery').append(`
                     <div class="img-wrapper">
@@ -96,7 +109,6 @@ $(document).ready(function () {
             }
         });
     }
-
 
 
 });
